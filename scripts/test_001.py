a863429e8a22546e39e2f9614676ce1dc53c7b72
wjh20181234 <awjh2014@163.com>
2018-05-27 16:42:25 +0800

    描述信息

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -6,9 +6,11 @@ class Test_001():
     @allure.step(title='添加测试标题001')
     @pytest.mark.parametrize('a',[1,2,3])
     def test01(self,a):
+        allure.attach('描述','我是01的描述信息')
         assert a !=2
 
     @allure.step(title='添加测试标题002')
     @pytest.mark.parametrize('a', [4, 5, 6])
     def test02(self, a):
+        allure.attach('描述','我是02的描述信息')
         assert a != 2
