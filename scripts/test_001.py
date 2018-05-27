3bd1421810af68b78579dc9bfbd5e08969dc70b6
wjh20181234 <awjh2014@163.com>
2018-05-27 16:19:09 +0800

    添加测试标题

--- scripts/test_001.py
+++ scripts/test_001.py
@@ -1,7 +1,14 @@
+import allure
 import pytest
 
 class Test_001():
 
+    @allure.step(title='添加测试标题001')
     @pytest.mark.parametrize('a',[1,2,3])
     def test01(self,a):
         assert a !=2
+
+    @allure.step(title='添加测试标题002')
+    @pytest.mark.parametrize('a', [4, 5, 6])
+    def test02(self, a):
+        assert a != 2
